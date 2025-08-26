a4c795d8944fe1da34b3b762964a6fbd82511d83
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2025-08-26 14:53:34 +0530

    Add files via upload

---
+++ a_power_b.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.Scanner;
+public class a_power_b {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+
+        int ans = 1;
+        for(int i = 1; i <=b; i++ ){
+            ans *= a;
+
+        }
+        System.out.println(ans);
+    }
+}
