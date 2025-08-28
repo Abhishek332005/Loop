7ae33bde18712f243a2e066a79156884b8e2af20
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2025-08-28 15:19:08 +0530

    Add files via upload

---
+++ first_multiple_of_5_7.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class first_multiple_of_5_7 {
+    public static void main(String[] args) {
+
+        int num = 1;
+
+        while(true){
+            if((num%5==0) && (num%7==0)){
+                System.out.println(num);
+                break;
+            }
+            num++;
+
+
+            }
+
+    }
+
+}
